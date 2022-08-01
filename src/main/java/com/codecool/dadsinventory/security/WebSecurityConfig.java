00413852f70690d6ee4f0b52792e4c9632a86499
MichałRogalski22 <michalrogalsk@gmail.com>
2022-08-02 00:38:45 +0200

    Mock user and implement password encryption

--- src/main/java/com/codecool/dadsinventory/security/WebSecurityConfig.java
+++ src/main/java/com/codecool/dadsinventory/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.codecool.dadsinventory.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public WebSecurityConfig(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,6 +34,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails dadUser = User.builder()
+                .username("micha")
+                .password(passwordEncoder.encode("password"))
+                .roles("DAD")
+                .build();
+        return new InMemoryUserDetailsManager(dadUser);
     }
 }
 
